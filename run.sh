54344e6738707c6087a91208df674e130a1942ec
Live session user <bmstover@gmail.com>
2018-11-15 05:23:58 +0000

    finished project, and finished implementing obscure skyrim reference

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #Ben Stover 
 
-export Head="<head>Welcome to Anises Cottage<head"
-export Body1="<body><Im just a poor old womand, dear. No need to trouble yourself with me<body>"
-export Body2="<body><a href = "/var/www/html/A/A.html">break into secret cellar?</a><body>"
-export head="<head><title>You found witch stuffs!</title</head>"
-export body="<body><Anise: Fool! None may know my secret!<body>"
+export Head="<head><title>Welcome to Anise's Cottage</title></head>"
+export Header="<h1>Anise: I'm just a poor old womand, dear. No need to trouble yourself with me<h1>"
+export Body="<body><a href = "/var/www/html/A/A.html">break into secret cellar?</a></body>"
+export head="<head><title>You found witch stuffs!</title></head>"
+export body="<body>Anise: Fool! None may know my secret!</body>"
 
 cd /var/www/html
 
 sudo touch index.html
 sudo chmod 777 index.html
 
-echo "<html> $Head $Body1 $Body2 <html>" > index.html
+echo "<html> $Head $Header $Body  <html>" > index.html
 
-mkdir A
+sudo rm -r A
+sudo mkdir A
 
 cd A
  
